abe9ed0504ad909f06cf7b229113574fb9ba44bf
Diego Cabre <diegocabre@gmail.com>
2024-03-07 19:23:09 -0300

    Cuarto Commit, Agregar la Fuente Roboto de google font

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,26 @@ $("#imagen2").hover(function () {
 $("#destacados-titulo").hover(function () {
   $("#destacados-titulo").animate({
     fontSize: "50px",
-    opacity: "1",
+    opacity: "3",
   });
   $("#destacados-titulo").css("color", "blue");
 });
+
+$("#destacados-titulo").mouseout(function () {
+  $("#destacados-titulo").animate({
+    fontSize: "30px",
+    opacity: "1",
+  })
+  $("#destacados-titulo").css("color", "white");
+})
+
+$("#contacto-titulo").hover(function () {
+  $("#contacto-titulo").css("color", "#b87201");
+});
+
+$("#contacto-titulo").mouseout(function () {
+  $("#contacto-titulo").css({
+    fontDecoration: "none",
+    opacity: "1",
+  })
+})
